e055a3bd2a2526c6bbb5d443c33fecd3fcdf17bc
Victor Lee <vic@Victors-MacBook-Pro.local>
2017-08-13 16:39:19 -1000

    adds new exercises

---
+++ exercises.js
@@ -0,0 +1,77 @@
+//while loops
+
+/*If you do not know how many times the code should run, using a while loop can help. Your code in the while loop will continue to run for as long as the condition is true.*/
+
+
+//example:
+
+/*var i = 5;
+
+while (i<10){	
+	console.log(i + " is next in line.");
+}*/
+
+//this while loop will continue to run since 5 will always meet the condition inside of the while loop ( 5 < 10);
+
+//Let's practice using some while loops.
+
+
+/*1. Use a while loop to create the following message:
+
+"Now serving 1."
+"Now serving 2."
+...
+...
+...
+"Now serving 11"*/
+
+
+
+
+
+/*2. Generate a multiplication table for the number 3 and print out the following:
+
+1 x 3 = 3
+2 x 3 = 6
+3 x 3 = 9
+...
+...
+...
+9 x 3 = 27
+
+*/
+
+
+
+/*3. Multiples of 3 and 5. Create a while loop that will loop through 100 numbers. Your job is to program the following:
+
+ a) if the number is multiple of 3, it should print "dev"
+ b) if the number is a multiple of 5, it should print "league"
+ c) if the number is a multiple of 3 and 5, it should print "devleague homie!"
+ d) if the number is neither a multiple of 3 and 5, it should print the number
+
+the print out should look like this:
+1
+2
+dev
+4
+league
+dev
+7
+8
+dev
+league
+11
+dev
+13
+14
+devleague homie!
+...
+...
+...*/
+
+
+
+
+
+
